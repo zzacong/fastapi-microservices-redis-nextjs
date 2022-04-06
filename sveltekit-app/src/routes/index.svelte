ce7e43c40c3b528312c7e64710b72897b432529b
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-04-06 21:03:01 +1000

    sveltekit setup

--- sveltekit-app/src/routes/index.svelte
+++ sveltekit-app/src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="h-screen pt-[40vh]">
+  <h1
+    class="bg-gradient-to-r from-blue-500 to-indigo-400 bg-clip-text text-center text-8xl font-bold text-transparent"
+  >
+    Welcome to SvelteKit
+  </h1>
+</div>
--- sveltekit-app/svelte.config.js
+++ sveltekit-app/svelte.config.js
@@ -1,15 +1,24 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          $styles: resolve('./src/styles'),
+          $components: resolve('./src/components')
+        }
+      }
+    }
+  }
 };
 
 export default config;
